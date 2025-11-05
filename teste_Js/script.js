75f4362aaafa1a74d931c04df15b28c5a4018cb4
samuel@SudoSu <samuel_mesquita@estudante.sesisenai.org.br>
2025-11-04 22:18:59 -0300

    Add files via upload

---
+++ teste_Js/script.js
@@ -0,0 +1,21 @@
+/*alert("Esta página tem um script rodando!");
+
+a=10;
+
+function calcular(a){
+    return 5*a;
+}
+alert(calcular(a))
+*/
+
+function conflict(){
+    window.location.href = "globalStorm.html";
+}
+
+function gta(){
+    window.location.href = "gtaVI.html";
+}
+
+function rdr(){
+    window.location.href = "rdr.html";
+}
